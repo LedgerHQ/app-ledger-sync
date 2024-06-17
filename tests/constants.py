4baa17d6331dc97cf1ddb56a81bc1b7da2db05e7
Charles-Edouard de la Vergne <charles-edouard.delavergne@ledger.fr>
2024-06-17 17:44:08 +0200

    Adapt tests
    - Remove User Approval in Seed Id
    - Adapt navigation after new Nano screens

--- tests/constants.py
+++ tests/constants.py
@@ -3,6 +3,6 @@ from ragger.navigator import NavInsID
 
 DEFAULT_TOPIC = "c96d450545ff2836204c29af291428a5bf740304978f5dfb0b4a261474192851"
 
-approve_instructions_nano = [NavInsID.RIGHT_CLICK, NavInsID.BOTH_CLICK]
+approve_instructions_nano = [NavInsID.RIGHT_CLICK, NavInsID.RIGHT_CLICK, NavInsID.BOTH_CLICK]
 
 approve_instructions_stax = [NavInsID.USE_CASE_CHOICE_CONFIRM]
