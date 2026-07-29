156dc008e0ed41809875c207c5953c43ae3fc969
Charles-Edouard de la Vergne <charles-edouard.delavergne@ledger.fr>
2026-07-29 17:16:26 +0200

    Remove useless WITH_PRIVACY_REPORT flag

--- src/ui/nbgl_display.c
+++ src/ui/nbgl_display.c
@@ -38,8 +38,6 @@
 #include "signer.h"
 #include "trusted_io.h"
 
-// #define WITH_PRIVACY_REPORT
-
 enum {
     TOKEN_PRIVACY = FIRST_USER_TOKEN,
     TOKEN_LOG_IN,
@@ -114,114 +112,6 @@ static void log_in_error_cb(nbgl_layout_t *layout, nbgl_buttonEvent_t event) {
 }
 #endif
 
-#ifdef WITH_PRIVACY_REPORT
-static void log_in_privacy_cb(void) {
-    ui_display_seed_id_command();
-}
-
-#define MAX_PAIRS 3
-static void log_in_cb(int token, uint8_t index) {
-    int error = -1;
-    static nbgl_layoutDescription_t layoutDescription = {0};
-    static nbgl_contentCenteredInfo_t centeredInfo = {0};
-    static nbgl_contentTagValue_t pairs[MAX_PAIRS];
-    static nbgl_genericContents_t contents = {0};
-    static nbgl_content_t contentsList = {0};
-    uint8_t nbPairs = 0;
-    int status = -1;
-
-    switch (token) {
-        case TOKEN_PRIVACY:
-            // add tag/values
-            pairs[nbPairs].item = "Data sharing";
-            pairs[nbPairs].value = "Anonymous device identifier.";
-            nbPairs++;
-            pairs[nbPairs].item = "Why it's needed";
-            pairs[nbPairs].value =
-                "Uniquely identifying your Ledger device grants access to your Ledger Wallet "
-                "features.";
-            nbPairs++;
-            pairs[nbPairs].item = "Data retrieval";
-            pairs[nbPairs].value = "Connected wallets at the time of sharing.";
-            nbPairs++;
-            // Values to be reviewed
-            contentsList.type = TAG_VALUE_LIST;
-            contentsList.content.tagValueList.pairs = pairs;
-            contentsList.content.tagValueList.nbPairs = nbPairs;
-            contentsList.content.tagValueList.wrapping = true;
-            // Generic page content
-            contents.contentsList = &contentsList;
-            contents.nbContents = 1;
-            nbgl_useCaseGenericConfiguration("Privacy Report", 0, &contents, log_in_privacy_cb);
-            break;
-
-        case TOKEN_LOG_IN:
-            error = seed_id_callback(index == 0);
-            if (error == -1) {
-                // add layout
-                layoutDescription.onActionCallback = log_in_error_cb;
-                layoutDescription.tapActionText = "Tap to dismiss";
-                layoutDescription.tapActionToken = TOKEN_LOG_IN;
-                layoutCtx = nbgl_layoutGet(&layoutDescription);
-                // add description
-                centeredInfo.text1 = "Error while connecting";
-                centeredInfo.text2 =
-                    "Try again. If this error repeats, contact Ledger Support at "
-                    "support.ledger.com";
-                centeredInfo.icon = &ICON_DENIED;
-                centeredInfo.style = LARGE_CASE_INFO;
-                status = nbgl_layoutAddCenteredInfo(layoutCtx, &centeredInfo);
-                if (status < 0) return;
-
-                // draw screen
-                nbgl_layoutDraw(layoutCtx);
-                nbgl_refreshSpecialWithPostRefresh(FULL_COLOR_CLEAN_REFRESH,
-                                                   POST_REFRESH_FORCE_POWER_ON);
-            } else if (!index) {
-                nbgl_useCaseStatus("Connection requested", true, ui_menu_main);
-            } else {
-                nbgl_useCaseStatus("Connection cancelled", false, ui_menu_main);
-            }
-            break;
-    }
-}
-
-int ui_display_seed_id_command(void) {
-    nbgl_layoutDescription_t layoutDescription = {0};
-    nbgl_contentCenteredInfo_t centeredInfo = {0};
-    nbgl_layoutChoiceButtons_t buttonInfo = {0};
-    int status = -1;
-
-    // add layout
-    layoutDescription.onActionCallback = log_in_cb;
-    layoutCtx = nbgl_layoutGet(&layoutDescription);
-    // add description
-    centeredInfo.text1 = "Connect with\nLedger Sync?";
-    centeredInfo.text2 = "Make sure to use Ledger Wallet only on a trusted phone or computer.";
-    centeredInfo.style = LARGE_CASE_INFO;
-    status = nbgl_layoutAddCenteredInfo(layoutCtx, &centeredInfo);
-    if (status < 0) return -1;
-
-    // Add top icon for Privacy Report
-    status =
-        nbgl_layoutAddTopRightButton(layoutCtx, &C_privacy_32px, TOKEN_PRIVACY, TUNE_TAP_CASUAL);
-    if (status < 0) return -1;
-
-    // Add choice buttons
-    buttonInfo.topText = "Connect";
-    buttonInfo.bottomText = "Don't connect";
-    buttonInfo.token = TOKEN_LOG_IN;
-    buttonInfo.style = ROUNDED_AND_FOOTER_STYLE;
-    buttonInfo.tuneId = TUNE_TAP_CASUAL;
-    status = nbgl_layoutAddChoiceButtons(layoutCtx, &buttonInfo);
-    if (status < 0) return -1;
-
-    // draw screen
-    nbgl_layoutDraw(layoutCtx);
-    nbgl_refreshSpecialWithPostRefresh(FULL_COLOR_CLEAN_REFRESH, POST_REFRESH_FORCE_POWER_ON);
-    return 0;
-}
-#else
 static void log_in_cb(bool confirm) {
     int error = -1;
     static nbgl_layoutDescription_t layoutDescription = {0};
@@ -277,7 +167,6 @@ int ui_display_seed_id_command(void) {
                        log_in_cb);
     return 0;
 }
-#endif
 
 // FLOW to display update member (Remove and add back needed instances):
 #ifdef SCREEN_SIZE_WALLET
